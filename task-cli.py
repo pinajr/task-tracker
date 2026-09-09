277061910131c0ba0e56ae271931df09e6cc5963
pinajr <122037160+pinajr@users.noreply.github.com>
2026-09-09 10:35:50 -0300

    Introduce the JSON file to store tasks
    Create a new file named `tasks.json` to store the tasks in JSON
    format. This file will be used to persist the tasks between program
    runs. The initial content of the file can be an empty JSON object `{}`
    or it can contain some predefined tasks.
    
    Two functions will be added to the `task-cli.py` file to read
    and write tasks to the `tasks.json` file.

--- task-cli.py
+++ task-cli.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 # Store the arguments passed to the script
 command_action = sys.argv[1]
@@ -20,11 +21,29 @@ def create_task(description):
         }
     )
 
-    print(f"Task added successfully (ID: {id}) {description}")
+    # Call the function to write the updated tasks to the JSON file
+    write_tasks_to_file()
+
+    print(f"Task added successfully (ID: {id})")
+
+# Save the updated tasks to the JSON file
+def write_tasks_to_file():
+    with open("tasks.json", "w", encoding="utf-8") as file:
+        json.dump(all_tasks, file, indent=4)
+
+# Load the tasks from the JSON file
+def load_tasks():
+    global all_tasks
+
+    with open("tasks.json", "r", encoding="utf-8") as file:
+        all_tasks = json.load(file)
+
+    # Convert the keys of the all_tasks dictionary to integers
+    all_tasks = {int(key): value for key, value in all_tasks.items()}
 
 def main():
-    global all_tasks
-    all_tasks = {}
+    # Call the function to load tasks from the JSON file
+    load_tasks()
 
     if command_action == 'add':
         description = argument_1
